701f3f308d17b55e64d597e4731f2ce9d6540b04
leafney <babycoolzx@126.com>
2023-04-21 09:15:55 +0800

    feat: mask methods

---
+++ mask.go
@@ -0,0 +1,29 @@
+package rose
+
+// MaskPhone
+func MaskPhone(phone string) string {
+	return ""
+}
+
+// MaskEmail
+func MaskEmail(email string) string {
+	return ""
+}
+
+// MaskPwd
+func MaskPwd(pwd string) string {
+
+	return ""
+}
+
+// MaskBankCard
+func MaskBankCard(card string) string {
+
+	return ""
+}
+
+// MaskIDCard
+func MaskIDCard(id string) string {
+
+	return ""
+}
