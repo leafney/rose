cf5ebd6c15a747ba2d7bd72c4b0a6b51c4ecedfd
leafney <babycoolzx@126.com>
2023-05-09 14:24:02 +0800

    docs: upt

--- bcrypt.go
+++ bcrypt.go
@@ -4,13 +4,13 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-// 通过Bcrypt生成哈希值
+// BcryptHash 通过Bcrypt生成哈希值
 func BcryptHash(str string) (newStr string, err error) {
 	newByte, err := bcrypt.GenerateFromPassword([]byte(str), bcrypt.DefaultCost)
 	return string(newByte), err
 }
 
-// 通过Bcrypt校验哈希值
+// BcryptCheck 通过Bcrypt校验哈希值
 func BcryptCheck(hashedPwd, pwd string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hashedPwd), []byte(pwd))
 }
