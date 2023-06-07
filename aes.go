c9ab1c75f950b90860b91fe293ff524860883177
leafney <babycoolzx@126.com>
2023-06-07 16:38:12 +0800

    feat: aes

--- aes.go
+++ aes.go
@@ -5,64 +5,19 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"encoding/base64"
+	"encoding/hex"
 	"errors"
 )
 
 /*
 	key为16,24,32位字符串，分别对应AES-128，AES-192，AES-256 加密方法
-	- AesCBCEncrypt
-	- AesCBCDecrypt
+	- AesCBCEncrypt (AesEncrypt)
+	- AesCBCDecrypt (AesDecrypt)
 	- AesCFBEncrypt
 	- AesCFBDecrypt
 	- AesECBEncrypt
 	- AesECBDecrypt
 */
-
-func AesEncrypt(key, data string) (string, error) {
-	keyBt := aesKeyPadding([]byte(key))
-	dataBt := []byte(data)
-
-	block, err := aes.NewCipher(keyBt)
-	if err != nil {
-		return "", err
-	}
-
-	// 加密块大小
-	blockSize := block.BlockSize()
-	//	填充
-	encryptBytes := pkcs7Padding(dataBt, blockSize)
-	// 初始化加密数据接收切片
-	crypted := make([]byte, len(encryptBytes))
-	// 使用cbc加密模式
-	blockMode := cipher.NewCBCEncrypter(block, keyBt[:blockSize])
-	// 执行加密
-	blockMode.CryptBlocks(crypted, encryptBytes)
-
-	return base64.StdEncoding.EncodeToString(crypted), nil
-}
-
-func AesDecrypt(key, data string) (string, error) {
-	dataBt, err := base64.StdEncoding.DecodeString(data)
-	if err != nil {
-		return "", err
-	}
-	keyBt := aesKeyPadding([]byte(key))
-
-	block, err := aes.NewCipher(keyBt)
-	if err != nil {
-		return "", err
-	}
-
-	blockSize := block.BlockSize()
-	blockMode := cipher.NewCBCDecrypter(block, keyBt[:blockSize])
-	crypted := make([]byte, len(dataBt))
-	blockMode.CryptBlocks(crypted, dataBt)
-	crypted, err = pkcs7UnPadding(crypted)
-	if err != nil {
-		return "", err
-	}
-	return string(crypted), nil
-}
 
 func aesKeyPadding(key []byte) []byte {
 	k := len(key)
@@ -98,4 +53,91 @@ func pkcs7UnPadding(data []byte) ([]byte, error) {
 	}
 	unPadding := int(data[length-1])
 	return data[:(length - unPadding)], nil
+}
+
+// --------
+
+func AesCBCEncrypt(key, data []byte) ([]byte, error) {
+	theKey := aesKeyPadding(key)
+	block, err := aes.NewCipher(theKey)
+	if err != nil {
+		return nil, err
+	}
+
+	// 加密块大小
+	blockSize := block.BlockSize()
+	//	填充
+	encryptBytes := pkcs7Padding(data, blockSize)
+	// 初始化加密数据接收切片
+	crypted := make([]byte, len(encryptBytes))
+	// 使用cbc加密模式
+	blockMode := cipher.NewCBCEncrypter(block, theKey[:blockSize])
+	// 执行加密
+	blockMode.CryptBlocks(crypted, encryptBytes)
+
+	return crypted, nil
+}
+
+func AesCBCDecrypt(key, data []byte) ([]byte, error) {
+	theKey := aesKeyPadding(key)
+	block, err := aes.NewCipher(theKey)
+	if err != nil {
+		return nil, err
+	}
+
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, theKey[:blockSize])
+	crypted := make([]byte, len(data))
+	blockMode.CryptBlocks(crypted, data)
+	crypted, err = pkcs7UnPadding(crypted)
+	if err != nil {
+		return nil, err
+	}
+	return crypted, nil
+}
+
+// AesEncrypt alias AesEncryptBase64
+func AesEncrypt(key, data string) string {
+	res, _ := AesEncryptBase64(key, data)
+	return res
+}
+
+// AesDecrypt alias AesDecryptBase64
+func AesDecrypt(key, data string) string {
+	res, _ := AesDecryptBase64(key, data)
+	return res
+}
+
+func AesEncryptHex(key, data string) (string, error) {
+	res, err := AesCBCEncrypt([]byte(key), []byte(data))
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(res), nil
+}
+
+func AesDecryptHex(key, data string) (string, error) {
+	theData, err := hex.DecodeString(data)
+	if err != nil {
+		return "", err
+	}
+	res, err := AesCBCDecrypt([]byte(key), theData)
+	return string(res), err
+}
+
+func AesEncryptBase64(key, data string) (string, error) {
+	res, err := AesCBCEncrypt([]byte(key), []byte(data))
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(res), nil
+}
+
+func AesDecryptBase64(key, data string) (string, error) {
+	theData, err := base64.StdEncoding.DecodeString(data)
+	if err != nil {
+		return "", err
+	}
+	res, err := AesCBCDecrypt([]byte(key), theData)
+	return string(res), err
 }
--- aes_test.go
+++ aes_test.go
@@ -11,10 +11,21 @@ package rose
 import "testing"
 
 func TestAesEncrypt(t *testing.T) {
-	pwdKey := "ABCDEFGHIJKLMNOP"
+	pwdKey := "ABCDEFGHIJKLMNOPXY"
 	origin := "Hello world"
-	enStr, err := AesEncrypt(pwdKey, origin)
+
+	//enStr, err := AesEncrypt([]byte(pwdKey), []byte(origin))
+	//t.Logf("RESULT [%v] err [%v]", string(enStr), err)
+	//deStr, err := AesDecrypt([]byte(pwdKey), enStr)
+	//t.Logf("RESULT [%v] err [%v]", string(deStr), err)
+
+	//enStr, err := AesEncryptHex(pwdKey, origin)
+	//t.Logf("RESULT [%v] err [%v]", enStr, err)
+	//deStr, err := AesDecryptHex(pwdKey, enStr)
+	//t.Logf("RESULT [%v] err [%v]", deStr, err)
+
+	enStr, err := AesEncryptBase64(pwdKey, origin)
 	t.Logf("RESULT [%v] err [%v]", enStr, err)
-	deStr, err := AesDecrypt(pwdKey, enStr)
+	deStr, err := AesDecryptBase64(pwdKey, enStr)
 	t.Logf("RESULT [%v] err [%v]", deStr, err)
 }
