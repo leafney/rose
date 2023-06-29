f2e5ff2a9ec4bfa86e64e5b88517bee38ba6ac92
leafney <babycoolzx@126.com>
2023-06-29 16:13:32 +0800

    feat: exec return byte

--- exec.go
+++ exec.go
@@ -34,42 +34,73 @@ func ExecCmdRunDirCtx(ctx context.Context, dir string, name string, arg ...strin
 	return cmd.Run()
 }
 
-func ExecCmdOut(name string, arg ...string) (string, error) {
+// -------------
+
+func ExecCmdOut(name string, arg ...string) (out string, err error) {
 	return ExecCmdOutCtx(context.Background(), name, arg...)
 }
 
-func ExecCmdOutDir(dir string, name string, arg ...string) (string, error) {
+func ExecCmdOutDir(dir string, name string, arg ...string) (out string, err error) {
 	return ExecCmdOutDirCtx(context.Background(), dir, name, arg...)
 }
 
-func ExecCmdOutCtx(ctx context.Context, name string, arg ...string) (string, error) {
+func ExecCmdOutCtx(ctx context.Context, name string, arg ...string) (out string, err error) {
 	return ExecCmdOutDirCtx(ctx, "", name, arg...)
 }
 
-func ExecCmdOutDirCtx(ctx context.Context, dir string, name string, arg ...string) (string, error) {
+func ExecCmdOutDirCtx(ctx context.Context, dir string, name string, arg ...string) (out string, err error) {
 	cmd := exec.CommandContext(ctx, name, arg...)
-	var out bytes.Buffer
-	cmd.Stdout = &out
+	var stdOut bytes.Buffer
+	cmd.Stdout = &stdOut
 	if !StrIsEmpty(dir) {
 		cmd.Dir = dir
 	}
-	err := cmd.Run()
-	return trimOut(out.Bytes()), err
+	err = cmd.Run()
+	out = trimOut(stdOut.Bytes())
+	return
 }
 
-func ExecCmdOutErr(name string, arg ...string) (string, string, error) {
+// -------------
+
+func ExecCmdOutB(name string, arg ...string) (out []byte, err error) {
+	return ExecCmdOutBCtx(context.Background(), name, arg...)
+}
+
+func ExecCmdOutDirB(dir string, name string, arg ...string) (out []byte, err error) {
+	return ExecCmdOutDirBCtx(context.Background(), dir, name, arg...)
+}
+
+func ExecCmdOutBCtx(ctx context.Context, name string, arg ...string) (out []byte, err error) {
+	return ExecCmdOutDirBCtx(ctx, "", name, arg...)
+}
+
+func ExecCmdOutDirBCtx(ctx context.Context, dir string, name string, arg ...string) (out []byte, err error) {
+	cmd := exec.CommandContext(ctx, name, arg...)
+	var stdOut bytes.Buffer
+	cmd.Stdout = &stdOut
+	if !StrIsEmpty(dir) {
+		cmd.Dir = dir
+	}
+	err = cmd.Run()
+	out = trimOutB(stdOut.Bytes())
+	return
+}
+
+// -------------
+
+func ExecCmdOutErr(name string, arg ...string) (out string, fail string, err error) {
 	return ExecCmdOutErrCtx(context.Background(), name, arg...)
 }
 
-func ExecCmdOutErrDir(dir string, name string, arg ...string) (string, string, error) {
+func ExecCmdOutErrDir(dir string, name string, arg ...string) (out string, fail string, err error) {
 	return ExecCmdOutErrDirCtx(context.Background(), dir, name, arg...)
 }
 
-func ExecCmdOutErrCtx(ctx context.Context, name string, arg ...string) (string, string, error) {
+func ExecCmdOutErrCtx(ctx context.Context, name string, arg ...string) (out string, fail string, err error) {
 	return ExecCmdOutErrDirCtx(ctx, "", name, arg...)
 }
 
-func ExecCmdOutErrDirCtx(ctx context.Context, dir string, name string, arg ...string) (string, string, error) {
+func ExecCmdOutErrDirCtx(ctx context.Context, dir string, name string, arg ...string) (out string, fail string, err error) {
 	cmd := exec.CommandContext(ctx, name, arg...)
 	var stdOut, stdErr bytes.Buffer
 	cmd.Stdout = &stdOut // 标准输出
@@ -77,73 +108,149 @@ func ExecCmdOutErrDirCtx(ctx context.Context, dir string, name string, arg ...st
 	if !StrIsEmpty(dir) {
 		cmd.Dir = dir
 	}
-	err := cmd.Run()
-	return trimOut(stdOut.Bytes()), trimOut(stdErr.Bytes()), err
+	err = cmd.Run()
+	out = trimOut(stdOut.Bytes())
+	fail = trimOut(stdErr.Bytes())
+	return
 }
+
+// -------------
+
+func ExecCmdOutErrB(name string, arg ...string) (out []byte, fail []byte, err error) {
+	return ExecCmdOutErrBCtx(context.Background(), name, arg...)
+}
+
+func ExecCmdOutErrDirB(dir string, name string, arg ...string) (out []byte, fail []byte, err error) {
+	return ExecCmdOutErrDirBCtx(context.Background(), dir, name, arg...)
+}
+
+func ExecCmdOutErrBCtx(ctx context.Context, name string, arg ...string) (out []byte, fail []byte, err error) {
+	return ExecCmdOutErrDirBCtx(ctx, "", name, arg...)
+}
+
+func ExecCmdOutErrDirBCtx(ctx context.Context, dir string, name string, arg ...string) (out []byte, fail []byte, err error) {
+	cmd := exec.CommandContext(ctx, name, arg...)
+	var stdOut, stdErr bytes.Buffer
+	cmd.Stdout = &stdOut // 标准输出
+	cmd.Stderr = &stdErr // 标准错误
+	if !StrIsEmpty(dir) {
+		cmd.Dir = dir
+	}
+	err = cmd.Run()
+	out = trimOutB(stdOut.Bytes())
+	fail = trimOutB(stdErr.Bytes())
+	return
+}
+
+// -------------
 
 // ExecCmdBashOut
 // "/bin/bash", "-c", "command"
-func ExecCmdBashOut(command string) (string, error) {
+func ExecCmdBashOut(command string) (out string, err error) {
 	return ExecCmdBashOutCtx(context.Background(), command)
 }
 
 // ExecCmdBashOutCtx
 // "/bin/bash", "-c", "command"
-func ExecCmdBashOutCtx(ctx context.Context, command string) (string, error) {
+func ExecCmdBashOutCtx(ctx context.Context, command string) (out string, err error) {
 	cmd := exec.CommandContext(ctx, Bash, SplitC, command)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	return trimOut(out.Bytes()), err
+	var stdOut bytes.Buffer
+	cmd.Stdout = &stdOut
+	err = cmd.Run()
+	out = trimOut(stdOut.Bytes())
+	return
 }
+
+// -------------
+
+func ExecCmdBashOutB(command string) (out []byte, err error) {
+	return ExecCmdBashOutBCtx(context.Background(), command)
+}
+
+func ExecCmdBashOutBCtx(ctx context.Context, command string) (out []byte, err error) {
+	cmd := exec.CommandContext(ctx, Bash, SplitC, command)
+	var stdOut bytes.Buffer
+	cmd.Stdout = &stdOut
+	err = cmd.Run()
+	out = trimOutB(stdOut.Bytes())
+	return
+}
+
+// -------------
 
 // ExecCmdBashOutErr
 // "/bin/bash", "-c", "command"
-func ExecCmdBashOutErr(command string) (string, string, error) {
+func ExecCmdBashOutErr(command string) (out string, fail string, err error) {
 	return ExecCmdBashOutErrCtx(context.Background(), command)
 }
 
 // ExecCmdBashOutErrCtx
 // "/bin/bash", "-c", "command"
-func ExecCmdBashOutErrCtx(ctx context.Context, command string) (string, string, error) {
+func ExecCmdBashOutErrCtx(ctx context.Context, command string) (out string, fail string, err error) {
 	cmd := exec.CommandContext(ctx, Bash, SplitC, command)
 	var stdOut, stdErr bytes.Buffer
 	cmd.Stdout = &stdOut // 标准输出
 	cmd.Stderr = &stdErr // 标准错误
-	err := cmd.Run()
-	return trimOut(stdOut.Bytes()), trimOut(stdErr.Bytes()), err
+	err = cmd.Run()
+	out = trimOut(stdOut.Bytes())
+	fail = trimOut(stdErr.Bytes())
+	return
 }
 
+// -------------
+
+func ExecCmdBashOutErrB(command string) (out []byte, fail []byte, err error) {
+	return ExecCmdBashOutErrBCtx(context.Background(), command)
+}
+
+func ExecCmdBashOutErrBCtx(ctx context.Context, command string) (out []byte, fail []byte, err error) {
+	cmd := exec.CommandContext(ctx, Bash, SplitC, command)
+	var stdOut, stdErr bytes.Buffer
+	cmd.Stdout = &stdOut // 标准输出
+	cmd.Stderr = &stdErr // 标准错误
+	err = cmd.Run()
+	out = trimOutB(stdOut.Bytes())
+	fail = trimOutB(stdErr.Bytes())
+	return
+}
+
+// -------------
+
 // ExecCmdCombinedOut CombinedOutput
-func ExecCmdCombinedOut(name string, arg ...string) (string, error) {
+func ExecCmdCombinedOut(name string, arg ...string) (out string, err error) {
 	return ExecCmdCombinedOutCtx(context.Background(), name, arg...)
 }
 
 // ExecCmdCombinedOutDir CombinedOutput
-func ExecCmdCombinedOutDir(dir string, name string, arg ...string) (string, error) {
+func ExecCmdCombinedOutDir(dir string, name string, arg ...string) (out string, err error) {
 	return ExecCmdCombinedOutDirCtx(context.Background(), dir, name, arg...)
 }
 
 // ExecCmdCombinedOutCtx CombinedOutput
-func ExecCmdCombinedOutCtx(ctx context.Context, name string, arg ...string) (string, error) {
+func ExecCmdCombinedOutCtx(ctx context.Context, name string, arg ...string) (out string, err error) {
 	return ExecCmdCombinedOutDirCtx(ctx, "", name, arg...)
 }
 
 // ExecCmdCombinedOutDirCtx CombinedOutput
-func ExecCmdCombinedOutDirCtx(ctx context.Context, dir string, name string, arg ...string) (string, error) {
+func ExecCmdCombinedOutDirCtx(ctx context.Context, dir string, name string, arg ...string) (out string, err error) {
 	cmd := exec.CommandContext(ctx, name, arg...)
 	if !StrIsEmpty(dir) {
 		cmd.Dir = dir
 	}
 	outBytes, err := cmd.CombinedOutput()
-
-	outStr := trimOut(outBytes)
-	return outStr, err
+	out = trimOut(outBytes)
+	return
 }
+
+// -------------
 
 // trimOut Clean the output and remove special characters
 func trimOut(input []byte) string {
 	return strings.TrimSpace(string(input))
+}
+
+func trimOutB(input []byte) []byte {
+	return bytes.TrimSpace(input)
 }
 
 // ----------------
