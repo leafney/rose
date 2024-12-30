eb9c85dbedfae1f5dea1822c06acfca1e12b71db
leafney <babycoolzx@126.com>
2024-12-30 18:06:34 +0800

    feat: dirExists crc32hash

--- crc32.go
+++ crc32.go
@@ -1,6 +1,7 @@
 package rose
 
 import (
+	"encoding/hex"
 	"hash/crc32"
 )
 
@@ -23,4 +24,14 @@ func Crc32Mod(key string, mod uint32) string {
 	val := crc32Int(key)
 	nVal := val % mod
 	return UInt32ToStr(nVal)
+}
+
+// CRC32Hash CRC32 校验
+func CRC32Hash(input string) string {
+	// 使用 CRC32 算法计算校验和
+	hash := crc32.NewIEEE()
+	hash.Write([]byte(input))
+
+	// 将计算出的 CRC32 校验和转为十六进制字符串
+	return hex.EncodeToString(hash.Sum(nil))
 }
--- dir.go
+++ dir.go
@@ -13,6 +13,22 @@ import (
 	"path/filepath"
 )
 
+// DirExists 判断目录路径是否存在
+func DirExists(path string) bool {
+	// 获取路径的状态信息
+	info, err := os.Stat(path)
+	if err != nil {
+		// 如果路径不存在或发生其他错误
+		if os.IsNotExist(err) {
+			return false
+		}
+		return false
+	}
+	// 判断是否为目录
+	return info.IsDir()
+}
+
+// Deprecated: use DirExists replaced
 // DIsExist 判断目录路径是否存在
 func DIsExist(path string) bool {
 	exist, err := DIsExistE(path)
@@ -22,6 +38,7 @@ func DIsExist(path string) bool {
 	return exist
 }
 
+// Deprecated: use DirExists replaced
 // DIsExistE 判断目录路径是否存在，抛出异常
 func DIsExistE(path string) (bool, error) {
 	dirPath := filepath.Dir(path)
@@ -35,8 +52,8 @@ func DIsExistE(path string) (bool, error) {
 	return info.IsDir(), nil
 }
 
-// DEnsurePathExist 确保目录路径存在 (判断路径是否存在，如果不存在则自动创建所需目录路径
-func DEnsurePathExist(path string) error {
+// DirExistsEnsure 确保目录路径存在 (判断路径是否存在，如果不存在则自动创建所需目录路径
+func DirExistsEnsure(path string) error {
 	dirPath := filepath.Dir(path)
 	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
 		err := os.MkdirAll(dirPath, os.ModePerm)
